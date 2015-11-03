cbab2b4e693d17885064ba9ebb0220fc1ecb3115
Mando Rodriguez <Armando.Rodriguez@utsa.edu>
2015-11-03 11:41:59 -0600

    1. Made a script to check for logged in samba users via the smbstatus command.

---
+++ logged_in_samba_users.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import os
+import pwd
+import pdb
+import argparse
+import time
+import shelve
+import psutil
+import subprocess
+"""
+
+simple script I use to check if a user is connect to samba and logs the
+last time seen into a python shelve. It uses a simple
+
+smbstatus -b
+
+command to get the logged in users.
+
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--database", type=str,
+                    help="Path to an sqlite database file", required=True)
+parser.add_argument("-p", "--program", type=str,
+                    help="Program string to match the ps output to", default="")
+parser.add_argument("-x", "--exclude", type=str, 
+                    nargs='*', help="List of users to exclude")
+
+args = parser.parse_args()
+
+
+grep_output = subprocess.check_output(["smbstatus", "-d"])
+
+
+curr_time = time.time()
+
+
+dbfile = args.database
+program_string = args.program
+exclude_list = args.exclude
+
+if args.exclude is None:
+    exclude_list = []
+else:
+    exclude_list = args.exclude
+
+logged_in_users = []
+
+for l in grep_output.split('\n'):
+
+    if program_string in l:
+
+        try:
+            user_id = l.split()[0]
+
+            if not user_id in logged_in_users and not user_id in exclude_list and not user_id.isdigit():
+                logged_in_users.append(user_id)
+        except:
+            pass
+
+#
+# caution, will add a '.db' to the file name
+#
+shv = shelve.open(dbfile, protocol=1, writeback=True)
+
+# loop through all users and log the last time they were logged in
+for user_id in logged_in_users:
+    
+    if not user_id in exclude_list:
+        
+        if shv.has_key(user_id):
+
+            #shv[user_id]['prev'] = shv[user_id]['last']
+            
+            shv[user_id]['last'] = curr_time
+
+        else:
+            #shv[user_id] = {'last' : curr_time, 'prev' : curr_time}
+            shv[user_id] = {'last' : curr_time}
+
+shv.close()
+
+
